3461f9d5005f48b715400be3cecd4762dc09c400
Ludmilaboffo <ludmilabboffo@hotmail.com>
2023-05-04 15:37:55 -0300

    Correccion del bug Picture Box. PB funcionando, desplegable de imagenes

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Imagen
+{
+	public Imagen()
+	{
+		public string ImagenUrl { get; set; };
+		public int idArticulo { get; set; };
+
+	}
+}
---
+++ negocio/ArticuloNegocio.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+using System.Data.SqlClient;
+using System.Collections;
+
+
+namespace negocio
+{
+    public class ArticuloNegocio
+    {
+        public List<Articulo> listar()
+        {
+            List<Articulo> lista = new List<Articulo>();
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;
+
+
+            try
+            {
+
+                conexion.ConnectionString = "server=.\\SQLEXPRESS; database=CATALOGO_P3_DB; integrated security=true";
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = "SELECT ID, CODIGO, NOMBRE, DESCRIPCION, IDMARCA, IDCATEGORIA, PRECIO FROM ARTICULOS";
+                comando.Connection = conexion;
+
+                conexion.Open();
+                lector = comando.ExecuteReader();
+
+                while (lector.Read())
+                {
+                    Articulo aux = new Articulo();
+                    aux.idArticulo = lector.GetInt32(0);
+                    aux.Codigo = (string)lector["Codigo"];
+                    aux.Nombre = (string)lector["Nombre"];
+                    aux.Descripcion = (string)lector["Descripcion"];
+                    //aux.Marca = (Marca)lector["IdMarca"];
+                    //aux.Categoria = (Categoria)lector["IdCategoria"];
+                    aux.Precio = (decimal)lector["Precio"];
+
+                    lista.Add(aux);
+
+                }
+
+                conexion.Close();
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+    }
+}
+
+
