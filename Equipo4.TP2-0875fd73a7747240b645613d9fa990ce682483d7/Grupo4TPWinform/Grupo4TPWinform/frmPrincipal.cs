1d6aed0a161d0cb3bb7322995e83a1657ea7d45d
Elizabeth Yamila Saavedra <117045923+Yamisaa@users.noreply.github.com>
2023-05-05 11:23:44 -0300

    programa ok

---
+++ Equipo4.TP2-0875fd73a7747240b645613d9fa990ce682483d7/Grupo4TPWinform/Grupo4TPWinform/frmPrincipal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grupo4TPWinform
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void listarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmListar ventanaListar = new frmListar();
+            ventanaListar.Show();
+        }
+    }
+}
---
+++ Equipo4.TP2-0875fd73a7747240b645613d9fa990ce682483d7/Grupo4TPWinform/dominio/Clase_Imagen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    class Clase_Imagen
+    {
+        public string ImagenUrl { get; set; }
+        public int idArticulo { get; set; }
+    }
+}
