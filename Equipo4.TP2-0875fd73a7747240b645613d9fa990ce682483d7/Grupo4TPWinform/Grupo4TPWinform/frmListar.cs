bf35ae9682c46e3614b8701648a52c73f1bc43ba
Elizabeth Yamila Saavedra <117045923+Yamisaa@users.noreply.github.com>
2023-05-05 11:36:38 -0300

    nuevo

---
+++ Equipo4.TP2-0875fd73a7747240b645613d9fa990ce682483d7/Grupo4TPWinform/Grupo4TPWinform/frmListar.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using dominio;
+using negocio;
+
+namespace Grupo4TPWinform
+{
+    public partial class frmListar : Form
+    {
+        public frmListar()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ArticuloNegocio negocio = new ArticuloNegocio();
+            dgvArticulos.DataSource = negocio.listar();
+        }
+
+        private void dgvArticulos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+
+    }
+}
